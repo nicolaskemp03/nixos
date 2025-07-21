b4966277f6e79b5af5714ece34d43eba2e4609ea
nico <nicolaskemp03@proton.me>
2025-07-21 17:17:56 -0400

    53 - 2025-07-18T20:47:46Z

--- modules/home-manager/affinity.nix
+++ modules/home-manager/affinity.nix
@@ -12,11 +12,11 @@ in
 {
   options.nico.affinity.enable = lib.mkEnableOption "Enable Affinity Suite.";
 
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      #inputs.affinity-nix.packages.x86_64-linux.photo
-      #inputs.affinity-nix.packages.x86_64-linux.designer
-      #inputs.affinity-nix.packages.x86_64-linux.publisher
-    ];
-  };
+  #config = lib.mkIf cfg.enable {
+  #home.packages = with pkgs; [
+  #inputs.affinity-nix.packages.x86_64-linux.photo
+  #inputs.affinity-nix.packages.x86_64-linux.designer
+  #inputs.affinity-nix.packages.x86_64-linux.publisher
+  #];
+  #};
 }
