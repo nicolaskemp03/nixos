3b2a6d960241f2958aae38eb0979fd5ebaa6d7fd
nico <nicolaskemp03@proton.me>
2025-07-22 23:21:33 -0400

    99 - 2025-07-23T03:21:31Z

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -204,4 +204,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  boot.kernelParams = [ "video=DP-1:2560x1440@144" ];
 }
