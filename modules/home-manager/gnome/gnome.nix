bd606fd5a9206d4d40415c8b6ee0cf98d28c926e
nico <nicolaskemp03@proton.me>
2025-07-23 17:01:33 -0400

    120 - 2025-07-23T21:01:32Z

--- modules/home-manager/gnome/gnome.nix
+++ modules/home-manager/gnome/gnome.nix
@@ -24,8 +24,8 @@ in
       enable = true;
 
       iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.papirus-icon-theme;
+        name = "Zafiro-icons-Dark";
+        package = pkgs.zafiro-icons;
       };
 
       theme = {
