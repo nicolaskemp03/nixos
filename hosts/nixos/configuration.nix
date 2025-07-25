8c6a287a6947dfd6ea52d3522e720341c255382d
nico <nicolaskemp03@proton.me>
2025-07-25 10:40:25 -0400

    131 - 2025-07-25T14:40:24Z

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -57,6 +57,17 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
+  };
+
   home-manager = {
     extraSpecialArgs = { inherit inputs paths; };
     users = {
