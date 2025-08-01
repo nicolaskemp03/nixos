810e811b128210525534931f3a0daaee6b1018c7
nico <nicolaskemp03@proton.me>
2025-08-01 16:59:34 -0400

    26 - 2025-08-01T20:59:33Z

--- modules/nixos/amdgpu.nix
+++ modules/nixos/amdgpu.nix
@@ -15,12 +15,10 @@ in
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        amdvlk # AMD's official Vulkan ICD
         rocmPackages.clr.icd # For OpenCL
       ];
       extraPackages32 = with pkgs.pkgsi686Linux; [
         # AMDVLK also has a 32-bit version which can be useful
-        amdvlk
       ];
     };
 
@@ -33,22 +31,21 @@ in
     };
 
     # I don't know what this was for and until knowing it it must be here.
-    /*
-      systemd.tmpfiles.rules =
-         let
-           rocmEnv = pkgs.symlinkJoin {
-             name = "rocm-combined";
-             paths = with pkgs.rocmPackages; [
-               rocblas
-               hipblas
-               clr
-             ];
-           };
-         in
-         [
-           "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
-         ];
-    */
+
+    systemd.tmpfiles.rules =
+      let
+        rocmEnv = pkgs.symlinkJoin {
+          name = "rocm-combined";
+          paths = with pkgs.rocmPackages; [
+            rocblas
+            hipblas
+            clr
+          ];
+        };
+      in
+      [
+        "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+      ];
 
     #environment.systemPackages = [ pkgs.lact ];
     #systemd.packages = [ pkgs.lact ];
