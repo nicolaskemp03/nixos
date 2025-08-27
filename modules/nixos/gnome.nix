f42175dd4559b2523ff6833879684490022fd128
nico <nicolaskemp03@proton.me>
2025-08-27 19:50:38 -0400

    79 - 2025-08-27T02:30:12Z

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -55,196 +55,190 @@ in
 
     /*
       systemd.services.gdm-setup-monitors = {
-         before = [ "display-manager.service" ];
-         wantedBy = [ "display-manager.service" ];
+            before = [ "display-manager.service" ];
+            wantedBy = [ "display-manager.service" ];
+            script = ''
+              if [[ ! -f /home/nico/.config/monitors.xml ]]; then
+                exit 0
+              fi
+              install -g gdm -o gdm /home/nico/.config/monitors.xml "${config.users.users.gdm.home}/.config"
+            '';
+          };
+
+       environment.systemPackages =
+         with pkgs;
+         [
+           xwayland-run
+           dconf2nix
+           gnome-software
+         ]
+         ++ (with pkgs.gnomeExtensions; [
+           appindicator
+           blur-my-shell
+           just-perfection
+           fuzzy-app-search
+           logo-menu
+           #quick-settings-tweaker #search for a replacement volume mixer
+           clipboard-indicator
+           forge
+           user-themes
+           caffeine
+           arcmenu
+           gsconnect
+         ]);
+       services.udev.packages = [ pkgs.gnome-settings-daemon ];
+
+       services.flatpak.enable = true;
+       #flathub repository
+       systemd.services.flatpak-repo = {
+         wantedBy = [ "multi-user.target" ];
+         path = [ pkgs.flatpak ];
          script = ''
-           if [[ ! -f /home/nico/.config/monitors.xml ]]; then
-             exit 0
-           fi
-           install -g gdm -o gdm /home/nico/.config/monitors.xml "${config.users.users.gdm.home}/.config"
+           flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
          '';
        };
-    */
-    environment.systemPackages =
-      with pkgs;
-      [
-        xwayland-run
-        dconf2nix
-        gnome-software
-      ]
-      ++ (with pkgs.gnomeExtensions; [
-        appindicator
-        blur-my-shell
-        just-perfection
-        fuzzy-app-search
-        logo-menu
-        #quick-settings-tweaker #search for a replacement volume mixer
-        clipboard-indicator
-        forge
-        user-themes
-        caffeine
-        arcmenu
-        gsconnect
-      ]);
-    services.udev.packages = [ pkgs.gnome-settings-daemon ];
 
-    services.flatpak.enable = true;
-    #flathub repository
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
+       #Flatpak packages to be installed
+       services.flatpak.packages = [
+         {
+           appId = "com.brave.Browser";
+           origin = "flathub";
+         }
+         "com.dec05eba.gpu_screen_recorder"
 
-    #Flatpak packages to be installed
-    services.flatpak.packages = [
-      {
-        appId = "com.brave.Browser";
-        origin = "flathub";
-      }
-      "com.dec05eba.gpu_screen_recorder"
+       ];
 
-    ];
+       home-manager.users.nico = {
+         home.packages = [
+           pkgs.gnome-tweaks
+         ];
 
-    home-manager.users.nico = {
-      home.packages = [
-        pkgs.gnome-tweaks
-      ];
+         gtk = {
+           enable = true;
 
-      gtk = {
-        enable = true;
+             iconTheme = {
+                    name = "Zafiro-icons-Dark";
+                    package = pkgs.zafiro-icons;
+                  };
 
-        /*
-          iconTheme = {
-                 name = "Zafiro-icons-Dark";
-                 package = pkgs.zafiro-icons;
-               };
+                  theme = {
+                    name = "Graphite";
+                    package = pkgs.graphite-gtk-theme.override {
+                      tweaks = [ "nord" ];
+                      themeVariants = [ "purple" ];
+                    };
+                  };
 
-               theme = {
-                 name = "Graphite";
-                 package = pkgs.graphite-gtk-theme.override {
-                   tweaks = [ "nord" ];
-                   themeVariants = [ "purple" ];
-                 };
-               };
-        */
-      };
+         };
 
-      home.activation = {
-        pre-switch-clean = ''
-          echo "Cleaning up conflicting gtkrc-2.0 file..."
-          rm -f ~/.gtkrc-2.0
-          rm -f ~/.gtkrc-2.0.backup
-        '';
-      };
+         home.activation = {
+           pre-switch-clean = ''
+             echo "Cleaning up conflicting gtkrc-2.0 file..."
+             rm -f ~/.gtkrc-2.0
+             rm -f ~/.gtkrc-2.0.backup
+           '';
+         };
 
-      /*
-        home.pointerCursor = {
-             gtk.enable = true;
-             name = "Afterglow-Recolored-Gruvbox-Purple";
-             package = pkgs.afterglow-cursors-recolored;
-             size = 24;
+           home.pointerCursor = {
+                gtk.enable = true;
+                name = "Afterglow-Recolored-Gruvbox-Purple";
+                package = pkgs.afterglow-cursors-recolored;
+                size = 24;
+              };
+
+         dconf.settings = {
+
+           #Gnome Theming
+             "org/gnome/desktop/interface" = {
+                    color-scheme = "prefer-dark";
+                    enable-hot-corners = false;
+                  };
+
+             "org/gnome/shell/extensions/user-theme" = {
+                  name = "Graphite";
+                };
+
+             "org/gnome/desktop/background" = {
+                    picture-uri = "/home/nico/nixos-config/modules/home-manager/gnome/IF_Background_Cachi.jpg";
+                    picture-uri-dark = "/home/nico/nixos-config/modules/home-manager/gnome/IF_Background_Cachi.jpg";
+                  };
+
+           #Peripherals
+           "org/gnome/desktop/peripherals/touchpad" = {
+             click-method = "areas";
+             tap-to-click = true;
+             two-finger-scrolling-enabled = true;
            };
-      */
-      dconf.settings = {
 
-        #Gnome Theming
-        /*
-          "org/gnome/desktop/interface" = {
-                 color-scheme = "prefer-dark";
-                 enable-hot-corners = false;
-               };
-        */
-        /*
-          "org/gnome/shell/extensions/user-theme" = {
-               name = "Graphite";
-             };
-        */
-        /*
-          "org/gnome/desktop/background" = {
-                 picture-uri = "/home/nico/nixos-config/modules/home-manager/gnome/IF_Background_Cachi.jpg";
-                 picture-uri-dark = "/home/nico/nixos-config/modules/home-manager/gnome/IF_Background_Cachi.jpg";
-               };
-        */
-        #Peripherals
-        "org/gnome/desktop/peripherals/touchpad" = {
-          click-method = "areas";
-          tap-to-click = true;
-          two-finger-scrolling-enabled = true;
-        };
+           "org/gnome/settings-daemon/plugins/power" = {
+             power-button-action = "nothing";
+             #sleep-inactive-ac-type = "nothing";
+             #sleep-inactive-battery-type = "nothing";
+           };
 
-        "org/gnome/settings-daemon/plugins/power" = {
-          power-button-action = "nothing";
-          #sleep-inactive-ac-type = "nothing";
-          #sleep-inactive-battery-type = "nothing";
-        };
+           #Shortcuts
+           "org/gnome/desktop/wm/keybindings" = {
+             # Erasing super + space shortcuts and setting other shortcuts
+             switch-input-source = [ ];
+             switch-input-source-backward = [ ];
+             close = "['<Control>q']";
+           };
 
-        #Shortcuts
-        "org/gnome/desktop/wm/keybindings" = {
-          # Erasing super + space shortcuts and setting other shortcuts
-          switch-input-source = [ ];
-          switch-input-source-backward = [ ];
-          close = "['<Control>q']";
-        };
+             "/org/gnome/shell/keybindings" = {
+                  show-screenshot-ui = "['<Shift><Super>s']";
 
-        /*
-          "/org/gnome/shell/keybindings" = {
-               show-screenshot-ui = "['<Shift><Super>s']";
+                };
 
-             };
-        */
+           "org/gnome/settings-daemon/plugins/media-keys" = {
+             home = [ "<Super>e" ];
+             custom-keybindings = [
+               "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+               "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+               "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+               "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+             ];
+           };
 
-        "org/gnome/settings-daemon/plugins/media-keys" = {
-          home = [ "<Super>e" ];
-          custom-keybindings = [
-            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
-            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
-          ];
-        };
+           "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+             name = "kitty";
+             command = "kitty";
+             binding = "<Super>t";
+           };
 
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-          name = "kitty";
-          command = "kitty";
-          binding = "<Super>t";
-        };
+           "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+             name = "rofi";
+             command = "rofi -normal-window -show combi -combi-modes \"window,drun,run\" -modes combi";
+             binding = "<Super>space";
+           };
 
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-          name = "rofi";
-          command = "rofi -normal-window -show combi -combi-modes \"window,drun,run\" -modes combi";
-          binding = "<Super>space";
-        };
+           "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+             name = "rebuild";
+             command = "kitty -- bash -c \"rebuild ; echo \\\"Press enter to close this window...\\\" ; read ans\"";
+             binding = "<Super>r";
+           };
 
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-          name = "rebuild";
-          command = "kitty -- bash -c \"rebuild ; echo \\\"Press enter to close this window...\\\" ; read ans\"";
-          binding = "<Super>r";
-        };
+           "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+             name = "firefox";
+             command = "firefox";
+             binding = "<Super>f";
+           };
 
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
-          name = "firefox";
-          command = "firefox";
-          binding = "<Super>f";
-        };
+           #Extension enabling
+           "org/gnome/shell" = {
+             disable-user-extension = false;
+             enabled-extensions = [
+               "user-theme@gnome-shell-extensions.gcampax.github.com"
+               "appindicatorsupport@rgcjonas.gmail.com"
+               "blur-my-shell@aunetx"
+               "clipboard-indicator@tudmotu.com"
+               "gnome-fuzzy-app-search@gnome-shell-extensions.Czarlie.gitlab.com"
+               "just-perfection-desktop@just-perfection"
+               "logomenu@aryan_k"
+             ];
+           };
+         };
 
-        #Extension enabling
-        "org/gnome/shell" = {
-          disable-user-extension = false;
-          enabled-extensions = [
-            "user-theme@gnome-shell-extensions.gcampax.github.com"
-            "appindicatorsupport@rgcjonas.gmail.com"
-            "blur-my-shell@aunetx"
-            "clipboard-indicator@tudmotu.com"
-            "gnome-fuzzy-app-search@gnome-shell-extensions.Czarlie.gitlab.com"
-            "just-perfection-desktop@just-perfection"
-            "logomenu@aryan_k"
-          ];
-        };
-      };
-
-    };
+       };
+    */
   };
 }
