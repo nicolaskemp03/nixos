2e2a336c77221001fd44ea8f7c09c3de3be9b8d7
nico <nicolaskemp03@proton.me>
2025-08-27 22:02:12 -0400

    82 - 2025-08-28T02:02:10Z

--- modules/nixos/base_configuration.nix
+++ modules/nixos/base_configuration.nix
@@ -81,6 +81,25 @@ in
     NH_FLAKE = "/home/nico/nixos-config";
   };
 
+  services.flatpak.enable = true;
+  #flathub repository
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
+  #Flatpak packages to be installed
+  services.flatpak.packages = [
+    {
+      appId = "com.brave.Browser";
+      origin = "flathub";
+    }
+    "com.dec05eba.gpu_screen_recorder"
+
+  ];
   programs.ssh.startAgent = mkDefault false;
 
   # Allow unfree packages
