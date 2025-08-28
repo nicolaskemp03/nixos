af363676d1b26e13a95b0b7c165b7b4e80a30c1f
nico <nicolaskemp03@proton.me>
2025-08-27 20:20:34 -0400

    80 - 2025-08-28T00:12:53Z

--- modules/nixos/theme.nix
+++ modules/nixos/theme.nix
@@ -15,5 +15,6 @@ in
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-dawn.yaml";
     stylix.targets.qt.enable = true;
+    hm.stylix.targets.vesktop.enable = true;
   };
 }
