fcadfcdbfb545f1603e0498d1c4c300542a9cea6
nico <nicolaskemp03@proton.me>
2025-09-07 17:42:55 -0300

    93 - 2025-09-07T20:42:54Z

--- modules/home-manager/base_configuration.nix
+++ modules/home-manager/base_configuration.nix
@@ -10,20 +10,5 @@
 
   config = {
 
-    home.packages = with pkgs; [
-      (import "${paths.derivations}/bg-run.nix" { inherit pkgs; })
-      trashy
-      vlc
-    ];
-
-    xdg.desktopEntries = {
-      "Rebuild" = {
-        name = "Rebuild";
-        genericName = "Rebuild NixOS";
-        exec = ''bash -c "rebuild ; echo \\"Press enter to close this window...\\" ; read ans" '';
-        terminal = true;
-        icon = "utilities-terminal";
-      };
-    };
   };
 }
